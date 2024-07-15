ee5471ad5fa9a60667e0674c6246061cde690d17
Roberto Uriona <roberto.uriona@jalasoft.com>
2024-07-15 00:22:54 -0400

    add schedule list of matches by round

--- src/js/matches.js
+++ src/js/matches.js
@@ -7,21 +7,31 @@ import ScheduleList from "./ScheduleList.mjs";
  * using getParam() function
  */
 document.addEventListener("DOMContentLoaded", async () => {
-  const leagueId = getParam("league");
-  const season = getParam("season");
-  const dataSource = new ExternalServices();
-  const scheduleDataByRound = await dataSource.getScheduleByRound(
-    leagueId,
-    season,
-  );
-  const numberOfRoundsBySeason = await dataSource.getNumberOfRoundsBySeason(
-    leagueId,
-    season,
-  );
-  setLocalStorage("rounds-by-season", numberOfRoundsBySeason);
+  const firstLoad = localStorage.getItem("firstLoad");
 
-  const scheduleList = new ScheduleList(leagueId, season, scheduleDataByRound);
-  await scheduleList.init();
+  if (firstLoad === "false") {
+    const leagueId = getParam("league");
+    const season = getParam("season");
+    const dataSource = new ExternalServices();
+    const scheduleDataByRound = await dataSource.getScheduleByRound(
+      leagueId,
+      season,
+    );
+    const numberOfRoundsBySeason = await dataSource.getNumberOfRoundsBySeason(
+      leagueId,
+      season,
+    );
+    setLocalStorage("rounds-by-season", numberOfRoundsBySeason);
+
+    const scheduleList = new ScheduleList(
+      leagueId,
+      season,
+      scheduleDataByRound,
+    );
+    await scheduleList.init();
+
+    localStorage.setItem("firstLoad", true);
+  }
 });
 
 const matchdaySelect = document.querySelector("#select-matchday");
@@ -30,6 +40,7 @@ matchdaySelect.addEventListener("input", async (e) => {
   const leagueId = getParam("league");
   const season = getParam("season");
   const round = e.target.value;
+  localStorage.setItem("current-round", round);
   const dataSource = new ExternalServices();
   const newScheduleData = await dataSource.getScheduleByRound(
     leagueId,
